909bb3b1e78026914bf7decac85e4ee11e1f92bf
zaytsev_dv <d.zaytsev@pulsiteam.ru>
2025-01-20 21:26:54 +0300

    убрал лишние сущности

--- noteBackendApp/api/handlers/handler.go
+++ noteBackendApp/api/handlers/handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"net/http"
+	noteApiDTO "noteBackendApp/api/openapi"
 	noteDao "noteBackendApp/internal/dao"
 	noteDomain "noteBackendApp/internal/domain"
 	"time"
@@ -17,21 +18,27 @@ type NoteApi struct {
 func (api *NoteApi) SaveNote(context *gin.Context) {
 	context.Header("Content-Type", "application/json")
 	decoder := json.NewDecoder(context.Request.Body)
-	var requestEntity noteDomain.CreateNoteRequest
+	var requestEntity noteApiDTO.CreateNoteRequest
 	err := decoder.Decode(&requestEntity)
 	if err != nil {
-		var responseError = noteDomain.ResponseError{
-			Timestamp:    time.Now().String(),
-			Status:       http.StatusBadRequest,
-			BusinessCode: "-",
-			Error:        err.Error(),
-			Path:         context.Request.URL.Path,
+		errorMsg := err.Error()
+		nowString := time.Now().String()
+		httpStatus := http.StatusBadRequest
+		var responseError = noteApiDTO.ErrorResponse{
+			Timestamp: &nowString,
+			Status:    &httpStatus,
+			Error:     &errorMsg,
+			Path:      &context.Request.URL.Path,
 		}
 		context.Status(http.StatusBadRequest)
 		json.NewEncoder(context.Writer).Encode(responseError)
 	}
-	entity := api.db.Save(noteDomain.NoteEntity{Id: uuid.New().String(), Name: requestEntity.Name, Link: requestEntity.Link})
-	var response = noteDomain.NoteResponse{Id: entity.Id, Name: entity.Name, Link: entity.Link}
+	entity := api.db.Save(noteDomain.NoteEntity{
+		Id:   uuid.New().String(),
+		Name: *requestEntity.Name,
+		Link: *requestEntity.Link,
+	})
+	var response = noteApiDTO.NoteResponse{Id: &entity.Id, Name: &entity.Name, Link: &entity.Link}
 	json.NewEncoder(context.Writer).Encode(response)
 }
 
@@ -39,17 +46,19 @@ func (api *NoteApi) GetNoteById(context *gin.Context, id string) {
 	context.Header("Content-Type", "application/json")
 	obj, err := api.db.GetById(id)
 	if err != nil {
-		var responseError = noteDomain.ResponseError{
-			Timestamp:    time.Now().String(),
-			Status:       http.StatusNotFound,
-			BusinessCode: "-",
-			Error:        err.Error(),
-			Path:         context.Request.URL.Path,
+		errorMsg := err.Error()
+		nowString := time.Now().String()
+		httpStatus := http.StatusNotFound
+		var responseError = noteApiDTO.ErrorResponse{
+			Timestamp: &nowString,
+			Status:    &httpStatus,
+			Error:     &errorMsg,
+			Path:      &context.Request.URL.Path,
 		}
 		context.Status(http.StatusNotFound)
 		json.NewEncoder(context.Writer).Encode(responseError)
 	} else {
-		var response = noteDomain.NoteResponse{Id: obj.Id, Name: obj.Name, Link: obj.Link}
+		var response = noteApiDTO.NoteResponse{Id: &obj.Id, Name: &obj.Name, Link: &obj.Link}
 		json.NewEncoder(context.Writer).Encode(response)
 	}
 }
--- noteBackendApp/internal/domain/model.go
+++ noteBackendApp/internal/domain/model.go
@@ -5,22 +5,3 @@ type NoteEntity struct {
 	Name string
 	Link string
 }
-
-type CreateNoteRequest struct {
-	Name string `json:"name"`
-	Link string `json:"link,optional"`
-}
-
-type NoteResponse struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-	Link string `json:"link,optional"`
-}
-
-type ResponseError struct {
-	Timestamp    string `json:"timestamp"`
-	Status       int    `json:"status"`
-	BusinessCode string `json:"businessCode,omitempty"`
-	Error        string `json:"error"`
-	Path         string `json:"path"`
-}
