df917b32c62cffcce23811f873771e27ba47cda2
zaytsev_dv <d.zaytsev@pulsiteam.ru>
2025-01-21 09:08:58 +0300

    рефакторинг

--- noteBackendApp/api/handlers/handler.go
+++ noteBackendApp/api/handlers/handler.go
@@ -15,21 +15,23 @@ type NoteApi struct {
 	db *noteDao.InMemoryNoteRepository
 }
 
+func getErrorDto(err string, status int, context *gin.Context) noteApiDTO.ErrorResponse {
+	nowString := time.Now().String()
+	return noteApiDTO.ErrorResponse{
+		Timestamp: &nowString,
+		Status:    &status,
+		Error:     &err,
+		Path:      &context.Request.URL.Path,
+	}
+}
+
 func (api *NoteApi) SaveNote(context *gin.Context) {
 	context.Header("Content-Type", "application/json")
 	decoder := json.NewDecoder(context.Request.Body)
 	var requestEntity noteApiDTO.CreateNoteRequest
 	err := decoder.Decode(&requestEntity)
 	if err != nil {
-		errorMsg := err.Error()
-		nowString := time.Now().String()
-		httpStatus := http.StatusBadRequest
-		var responseError = noteApiDTO.ErrorResponse{
-			Timestamp: &nowString,
-			Status:    &httpStatus,
-			Error:     &errorMsg,
-			Path:      &context.Request.URL.Path,
-		}
+		var responseError = getErrorDto(err.Error(), http.StatusBadRequest, context)
 		context.Status(http.StatusBadRequest)
 		json.NewEncoder(context.Writer).Encode(responseError)
 	}
@@ -52,15 +54,7 @@ func (api *NoteApi) GetNoteById(context *gin.Context, id string) {
 	context.Header("Content-Type", "application/json")
 	obj, err := api.db.GetById(id)
 	if err != nil {
-		errorMsg := err.Error()
-		nowString := time.Now().String()
-		httpStatus := http.StatusNotFound
-		var responseError = noteApiDTO.ErrorResponse{
-			Timestamp: &nowString,
-			Status:    &httpStatus,
-			Error:     &errorMsg,
-			Path:      &context.Request.URL.Path,
-		}
+		var responseError = getErrorDto(err.Error(), http.StatusNotFound, context)
 		context.Status(http.StatusNotFound)
 		json.NewEncoder(context.Writer).Encode(responseError)
 	} else {
