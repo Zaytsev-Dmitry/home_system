759c7470aaea707cbd4bc1af25262e9a2389ca81
zaytsev_dv <d.zaytsev@pulsiteam.ru>
2025-01-21 20:00:15 +0300

    set main middleware

--- noteBackendApp/cmd/main.go
+++ noteBackendApp/cmd/main.go
@@ -13,11 +13,16 @@ func main() {
 	startMessage := "Note backend ver 1.0"
 	fmt.Printf("%s!\n", startMessage)
 	router, apiInterface := initAPI()
+	router.Use(commonMiddleware)
 	api.RegisterHandlers(router, apiInterface)
 	log.Println("Starting server on :8080")
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
+}
+
+func commonMiddleware(context *gin.Context) {
+	context.Header("Content-Type", "application/json")
 }
 
 func initAPI() (router *gin.Engine, serverInterface api.ServerInterface) {
