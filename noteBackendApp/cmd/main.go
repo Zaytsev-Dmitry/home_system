f15ef7ed7a044de4411f7d48de83caac28243cda
zaytsev_dv <d.zaytsev@pulsiteam.ru>
2025-01-21 11:43:16 +0300

    update

--- noteBackendApp/cmd/main.go
+++ noteBackendApp/cmd/main.go
@@ -12,13 +12,15 @@ import (
 func main() {
 	startMessage := "Note backend ver 1.0"
 	fmt.Printf("%s!\n", startMessage)
-	noteRepository := noteDao.NewInMemoryNoteRepository()
-	router := gin.Default()
-	noteApi := noteHandler.NewNoteBackendApi(noteRepository)
-	api.RegisterHandlers(router, noteApi)
-
+	router, apiInterface := initAPI()
+	api.RegisterHandlers(router, apiInterface)
 	log.Println("Starting server on :8080")
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
+}
+
+func initAPI() (router *gin.Engine, serverInterface api.ServerInterface) {
+	noteRepository := noteDao.NewInMemoryNoteRepository()
+	return gin.Default(), noteHandler.NewNoteBackendApi(noteRepository)
 }
