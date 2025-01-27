20cc1e7e4c0def4f845c4a30c131e46b789aa2a8
zaytsev_dv <d.zaytsev@pulsiteam.ru>
2025-01-27 17:24:15 +0300

    raw commit

--- telegramClient/cmd/main.go
+++ telegramClient/cmd/main.go
@@ -2,34 +2,63 @@ package main
 
 import (
 	"fmt"
-	"github.com/mymmrac/telego"
-	th "github.com/mymmrac/telego/telegohandler"
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/callbackquery"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/message"
 	"gopkg.in/yaml.v3"
 	"log"
 	"os"
 	"telegramCLient/config"
-	"telegramCLient/internal/handler"
+	"time"
 )
 
-func main() {
-	bh, bot := initNoteTGBot()
-	defer bh.Stop()
-	defer bot.StopLongPolling()
+func initNoteTGBot(config *config.AppConfig) *ext.Updater {
+	token := config.BotToken
+	if token == "" {
+		panic("TOKEN environment variable is empty")
+	}
 
-	handler.HandleStartCommand(bh)
-	bh.Start()
+	// Create bot from environment value.
+	b, err := gotgbot.NewBot(token, nil)
+	if err != nil {
+		panic("failed to create new bot: " + err.Error())
+	}
+
+	// Create updater and dispatcher.
+	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
+		// If an error is returned by a handler, log it and continue going.
+		Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+			log.Println("an error occurred while handling update:", err.Error())
+			return ext.DispatcherActionNoop
+		},
+		MaxRoutines: ext.DefaultMaxRoutines,
+	})
+	updater := ext.NewUpdater(dispatcher, nil)
+
+	dispatcher.AddHandler(handlers.NewCommand("start", start))
+	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Equal("start_callback"), startCB))
+	dispatcher.AddHandler(handlers.NewMessage(message.Text, echo))
+
+	err = updater.StartPolling(b, &ext.PollingOpts{
+		DropPendingUpdates: true,
+		GetUpdatesOpts: &gotgbot.GetUpdatesOpts{
+			Timeout: 9,
+			RequestOpts: &gotgbot.RequestOpts{
+				Timeout: time.Second * 10,
+			},
+		},
+	})
+	if err != nil {
+		panic("failed to start polling: " + err.Error())
+	}
+	log.Printf("%s has been started...\n", b.User.Username)
+	return updater
 }
 
-func initNoteTGBot() (bh *th.BotHandler, bot *telego.Bot) {
-	appConfig := loadConfig("MODE")
-	bot, err := telego.NewBot(appConfig.BotToken, telego.WithDefaultDebugLogger())
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	updates, _ := bot.UpdatesViaLongPolling(nil)
-	bh, _ = th.NewBotHandler(bot, updates)
-	return bh, bot
+func main() {
+	initNoteTGBot(loadConfig("MODE")).Idle()
 }
 
 func loadConfig(env string) *config.AppConfig {
@@ -54,4 +83,35 @@ func loadConfig(env string) *config.AppConfig {
 
 	}
 	return &cfg
+}
+
+func start(b *gotgbot.Bot, ctx *ext.Context) error {
+	b.SendMessage(
+		ctx.Message.Chat.Id,
+		"Супер теперь надо зарегаться",
+		&gotgbot.SendMessageOpts{
+			ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+				InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+					{Text: "Вперед", CallbackData: "start_callback"},
+				}},
+			},
+		})
+	return nil
+}
+
+func echo(b *gotgbot.Bot, ctx *ext.Context) error {
+	_, err := b.SendMessage(ctx.Message.Chat.Id, "text", nil)
+	if err != nil {
+		return fmt.Errorf("failed to echo message: %w", err)
+	}
+	return nil
+}
+
+func startCB(b *gotgbot.Bot, ctx *ext.Context) error {
+	cb := ctx.Update.CallbackQuery
+	_, err2 := b.SendMessage(cb.Message.GetChat().Id, "text", nil)
+	if err2 != nil {
+		return fmt.Errorf("failed to edit start mfghfghessage text: %w", err2)
+	}
+	return nil
 }
--- telegramClient/go.mod
+++ telegramClient/go.mod
@@ -3,26 +3,12 @@ module telegramCLient
 go 1.23.0
 
 require (
-	github.com/mymmrac/telego v0.32.0
+	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.31
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/andybalholm/brotli v1.1.1 // indirect
-	github.com/bytedance/sonic v1.12.7 // indirect
-	github.com/bytedance/sonic/loader v0.2.2 // indirect
-	github.com/cloudwego/base64x v0.1.4 // indirect
-	github.com/fasthttp/router v1.5.4 // indirect
-	github.com/grbit/go-json v0.11.0 // indirect
-	github.com/klauspost/compress v1.17.11 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
-	github.com/savsgio/gotils v0.0.0-20240704082632-aef3928b8a38 // indirect
-	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.58.0 // indirect
-	github.com/valyala/fastjson v1.6.4 // indirect
-	golang.org/x/arch v0.6.0 // indirect
-	golang.org/x/sys v0.27.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
