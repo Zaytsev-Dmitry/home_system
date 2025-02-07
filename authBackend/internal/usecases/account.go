1abd13e5829ad697c9432e9687b90c9861a618c0
sladkiy_bubalex <zaytsev.dmitry9228@gmail.com>
2025-02-07 14:50:14 +0300

    refactoring

--- authBackend/internal/usecases/account.go
+++ authBackend/internal/usecases/account.go
@@ -10,11 +10,6 @@ import (
 	"net/http"
 )
 
-const (
-	TG  authSpec.CreateAccountRequestAccountType = "TG"
-	WEB authSpec.CreateAccountRequestAccountType = "WEB"
-)
-
 type AccountUseCase struct {
 	Keycloak       *keycloak.KeycloakClient
 	Repo           intefraces.AccountRepository
@@ -22,45 +17,66 @@ type AccountUseCase struct {
 }
 
 func (usecase *AccountUseCase) Register(request authSpec.CreateAccountRequest) (domain.Account, error, int) {
-	var status int
+	var status = http.StatusOK
 	var respErr error
 	var keycloakEntity keycloak.KeycloakEntity
+	var result domain.Account
 
-	if *request.AccountType == WEB {
-		respErr, keycloakEntity = usecase.Keycloak.RegisterAccount(request)
-		if respErr != nil {
-			if errors.Is(respErr, keycloak.Conflict409) {
-				//пользак уже есть в keycloak и соответственно в базе
-				utilities.GetLogger().Warn(respErr.Error())
-				user, getUserErr := usecase.Keycloak.GetUser(*request.Email)
-				keycloakEntity = user
-				if getUserErr != nil {
-					utilities.GetLogger().Error(respErr.Error())
-					status = http.StatusInternalServerError
-				}
-			} else {
-				utilities.GetLogger().Error(respErr.Error())
-				status = http.StatusInternalServerError
-			}
-		}
+	respErr, result = usecase.runBusinessLayout(request, respErr, keycloakEntity, result)
+	status = usecase.getStatus(respErr, status)
+	return result, respErr, status
+}
+
+func (usecase *AccountUseCase) getStatus(respErr error, status int) int {
+	if respErr != nil {
+		utilities.GetLogger().Error(respErr.Error())
+		status = http.StatusInternalServerError
 	}
+	return status
+}
+
+func (usecase *AccountUseCase) runBusinessLayout(request authSpec.CreateAccountRequest, respErr error, keycloakEntity keycloak.KeycloakEntity, result domain.Account) (error, domain.Account) {
+	respErr, keycloakEntity = usecase.getKeycloakUser(request)
+	if respErr == nil {
+		result = usecase.getDaoEntity(request, respErr, keycloakEntity)
+	}
+	if respErr == nil {
+		usecase.ProfileUsecase.Create(result, *request.TelegramUsername)
+	}
+	return respErr, result
+}
+
+func (usecase *AccountUseCase) getDaoEntity(request authSpec.CreateAccountRequest, respErr error, keycloakEntity keycloak.KeycloakEntity) domain.Account {
 	saved, respErr := usecase.Repo.Register(
 		domain.Account{
 			FirstName:  &keycloakEntity.FirstName,
 			LastName:   &keycloakEntity.LastName,
 			Email:      keycloakEntity.Email,
-			Type:       string(*request.AccountType),
 			TelegramId: request.TelegramId,
 			IsActive:   true,
 		},
 	)
-	if respErr != nil {
-		utilities.GetLogger().Error(respErr.Error())
-		status = http.StatusInternalServerError
-	} else {
-		usecase.ProfileUsecase.Create(saved, *request.TelegramUsername)
+	return saved
+}
+
+func (usecase *AccountUseCase) getKeycloakUser(request authSpec.CreateAccountRequest) (error, keycloak.KeycloakEntity) {
+	var keycloakEntity keycloak.KeycloakEntity
+	var err error
+
+	err, keycloakEntity = usecase.Keycloak.RegisterAccount(request)
+	if err != nil {
+		if errors.Is(err, keycloak.Conflict409) {
+			//пользак уже есть в keycloak и соответственно в базе
+			utilities.GetLogger().Warn(err.Error())
+			keycloakEntity, err = usecase.Keycloak.GetUser(*request.Email)
+			if err != nil {
+				utilities.GetLogger().Error(err.Error())
+			}
+		} else {
+			utilities.GetLogger().Error(err.Error())
+		}
 	}
-	return saved, respErr, status
+	return err, keycloakEntity
 }
 
 func (usecase *AccountUseCase) GetAccountIdByTgId(tgId int64) (accId int64) {
