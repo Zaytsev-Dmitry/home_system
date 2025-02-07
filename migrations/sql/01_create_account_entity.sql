80374adcae0c2b46bcc8b75bd9e4a143267c5471
sladkiy_bubalex <zaytsev.dmitry9228@gmail.com>
2025-02-07 16:07:20 +0300

    refactoring

--- migrations/sql/01_create_account_entity.sql
+++ migrations/sql/01_create_account_entity.sql
@@ -1,10 +1,13 @@
 CREATE TABLE accounts
 (
-    id         SERIAL PRIMARY KEY,
-    first_name varchar(100),
-    last_name  varchar(100),
-    login      varchar(100),
-    email      varchar(100),
-    type       varchar(10),
-    telegram_id int
+    id          SERIAL PRIMARY KEY,
+    first_name  varchar(100),
+    last_name   varchar(100),
+    username    varchar(100),
+    email       varchar(100),
+    telegram_id int,
+    keycloak_id text,
+    is_active   boolean
 );
+grant all privileges on table accounts to auth_user;
+grant all privileges on sequence accounts_id_seq to auth_user;
