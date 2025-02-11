3d2a01c3e3a971c89dd14b4d7fe3bac16c67793a
sladkiy_bubalex <zaytsev.dmitry9228@gmail.com>
2025-02-11 18:02:37 +0300

    вырубил не реализованные менюшки

--- telegramBotBackend/internal/handler/command/note.go
+++ telegramBotBackend/internal/handler/command/note.go
@@ -96,16 +96,16 @@ func (h *NoteCommandHandler) buildNotesKeyboard() models.ReplyMarkup {
 	kb := &models.InlineKeyboardMarkup{
 		InlineKeyboard: [][]models.InlineKeyboardButton{
 			{
-				{Text: "Добавить запись 📄", CallbackData: "add_note"},
+				//{Text: "Добавить запись 📄", CallbackData: "add_note"},
 			},
 			{
-				{Text: "Удалить записку ❌", CallbackData: "delete_note"},
+				//{Text: "Удалить записку ❌", CallbackData: "delete_note"},
 			},
 			{
 				{Text: "Просмотреть все записки 🗄️", CallbackData: "show_all_notes"},
 			},
 			{
-				{Text: "Поиск записки по названию 🔎", CallbackData: "show_note_by_name"},
+				//{Text: "Поиск записки по названию 🔎", CallbackData: "show_note_by_name"},
 			},
 			{
 				{Text: "Назад к меню 🤙", CallbackData: "open_menu"},
