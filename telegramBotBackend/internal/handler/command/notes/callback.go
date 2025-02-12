ecf8ad13db321c2775d0424dcc720fe811a16182
sladkiy_bubalex <zaytsev.dmitry9228@gmail.com>
2025-02-12 19:13:30 +0300

    реализация сохранения записок и отображения записок

--- telegramBotBackend/internal/handler/command/notes/callback.go
+++ telegramBotBackend/internal/handler/command/notes/callback.go
@@ -2,18 +2,46 @@ package notes
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"telegramCLient/internal/components/paginator"
+	"telegramCLient/internal/handler/loader"
+	"telegramCLient/internal/storage"
 	"telegramCLient/util"
 )
 
-func (h *NoteCommand) callback(ctx context.Context, b *bot.Bot, update *models.Update) {
+func (n *NoteCommand) addNoteCallback(ctx context.Context, b *bot.Bot, update *models.Update) {
+	//TODO вызывать logcommon action
+	n.component.Collect(ctx, b, update)
+}
+
+// TODO если ошибки то обработать
+func (n *NoteCommand) showAllNotesCallback(ctx context.Context, b *bot.Bot, update *models.Update) {
+	//TODO вызывать logcommon action
+	data := n.noteBackClient.GetAllNotesByAccount(update.CallbackQuery.From.ID)
+	var stringData []string
+	for _, value := range *data.Objects {
+		text := fmt.Sprintf(
+			loader.NoteCommandContentText,
+			*value.Id,
+			*value.Name,
+			*value.Description,
+		)
+		stringData = append(stringData, text)
+	}
+	paginator.NewPaginator().CreateAndRun(ctx, b, update, stringData, 5, "Закрыть ❌")
+}
+
+func (n *NoteCommand) showNotesKeyboardCallback(ctx context.Context, b *bot.Bot, update *models.Update) {
 	chatId, _ := util.GetChatAndMsgId(update)
-	b.SendMessage(
+	message, _ := b.SendMessage(
 		ctx,
 		&bot.SendMessageParams{
 			ChatID:      chatId,
 			Text:        "Выбери действие",
-			ReplyMarkup: h.buildNotesKeyboard(),
+			ReplyMarkup: n.buildNotesKeyboard(),
 		})
+	n.messageStorage.Add(chatId, *storage.NewMessage(message.ID, message.Text, 0, storage.BOT))
+	n.LogCommandAction(chatId, "start")
 }
--- telegramBotBackend/internal/handler/command/notes/note.go
+++ telegramBotBackend/internal/handler/command/notes/note.go
@@ -2,28 +2,30 @@ package notes
 
 import (
 	"context"
+	noteSpec "github.com/Zaytsev-Dmitry/home_system_open_api/noteServerBackend"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"telegramCLient/external"
 	"telegramCLient/internal/components/echo"
 	"telegramCLient/internal/dao"
 	"telegramCLient/internal/handler/command"
+	"telegramCLient/internal/handler/loader"
 	"telegramCLient/internal/storage"
 )
 
 type NoteCommand struct {
 	noteBackClient    *external.NoteBackendClient
-	echoComponent     *echo.Echo
+	component         *echo.Echo
 	dao               dao.TelegramBotDao
 	messageStorage    storage.Storage
 	ctx               context.Context
 	bot               *bot.Bot
-	callbackHandlerID string
+	callbackHandlerID []string
 	action            command.UserAction
 }
 
 func NewNotesCommand(action command.UserAction, st storage.Storage, bot *bot.Bot, ctx context.Context, d dao.TelegramBotDao, client *external.NoteBackendClient) *NoteCommand {
-	return &NoteCommand{
+	n := &NoteCommand{
 		noteBackClient: client,
 		dao:            d,
 		messageStorage: st,
@@ -31,10 +33,21 @@ func NewNotesCommand(action command.UserAction, st storage.Storage, bot *bot.Bot
 		bot:            bot,
 		action:         action,
 	}
+	textMeta := echo.TextMeta{
+		ConfirmText: loader.AddNoteConfirmCommandText,
+		StartText:   loader.AddNoteStartCommandText,
+	}
+	options := []echo.Option{
+		echo.WithMessageStorage(st),
+	}
+	n.component = echo.NewEcho(bot, n.getQuestions(), n.proceedResult, n.LogCommandAction, textMeta, options)
+	return n
 }
 
 func (n *NoteCommand) RegisterHandler() {
-	n.callbackHandlerID = n.bot.RegisterHandler(bot.HandlerTypeMessageText, n.GetName(), bot.MatchTypeExact, n.callback)
+	n.callbackHandlerID = append(n.callbackHandlerID, n.bot.RegisterHandler(bot.HandlerTypeMessageText, "/notes", bot.MatchTypeExact, n.showNotesKeyboardCallback))
+	n.callbackHandlerID = append(n.callbackHandlerID, n.bot.RegisterHandler(bot.HandlerTypeCallbackQueryData, "add_note", bot.MatchTypeExact, n.addNoteCallback))
+	n.callbackHandlerID = append(n.callbackHandlerID, n.bot.RegisterHandler(bot.HandlerTypeCallbackQueryData, "show_all_notes", bot.MatchTypeExact, n.showAllNotesCallback))
 }
 
 func (p *NoteCommand) LogCommandAction(userId int64, status string) {
@@ -42,8 +55,63 @@ func (p *NoteCommand) LogCommandAction(userId int64, status string) {
 }
 
 func (n *NoteCommand) ProceedUserAnswer(ctx context.Context, b *bot.Bot, update *models.Update) {
+	n.component.ProceedUserAnswer(ctx, b, update)
+}
+
+func (s *NoteCommand) proceedResult(result echo.Result) {
+	request := noteSpec.CreateNoteRequest{
+		TgId: &result.ChatId,
+	}
+	for i, answer := range result.Question {
+		if answer.FieldId == "name" {
+			request.Name = &result.Question[i].Answer
+		}
+		if answer.FieldId == "content" {
+			request.Description = &result.Question[i].Answer
+		}
+		if answer.FieldId == "link" {
+			request.Link = &result.Question[i].Answer
+		}
+	}
+	s.noteBackClient.Save(request)
+
+	s.bot.DeleteMessages(
+		s.ctx, &bot.DeleteMessagesParams{
+			ChatID:     result.ChatId,
+			MessageIDs: result.MessagesIds[0:len(result.MessagesIds)],
+		},
+	)
+	s.messageStorage.ClearAll(result.ChatId)
+
+	s.bot.SendMessage(
+		s.ctx,
+		&bot.SendMessageParams{
+			ChatID:      result.ChatId,
+			Text:        "Выбери действие",
+			ReplyMarkup: s.buildNotesKeyboard(),
+		})
 }
 
 func (n *NoteCommand) GetName() string {
 	return "/notes"
+}
+
+func (n *NoteCommand) getQuestions() []echo.CollectItem {
+	return []echo.CollectItem{
+		{
+			FieldId:   "name",
+			FieldName: "Название: ",
+			Content:   "какое название записки?",
+		},
+		{
+			FieldId:   "content",
+			FieldName: "Содержимое: ",
+			Content:   "напиши содержимое",
+		},
+		{
+			FieldId:   "link",
+			FieldName: "Ссылка: ",
+			Content:   "какая ссылка",
+		},
+	}
 }
