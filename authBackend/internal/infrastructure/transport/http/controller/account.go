5906690b784b2ca6fbcc63f88ce5ab385c8fa358
sladkiy_bubalex <zaytsev.dmitry9228@gmail.com>
2025-04-06 14:42:46 +0300

    fix transport infra part 3

--- authBackend/internal/infrastructure/transport/http/controller/account.go
+++ authBackend/internal/infrastructure/transport/http/controller/account.go
@@ -4,47 +4,17 @@ import (
 	"authBackend/internal/app/ports/in/delegate"
 	daoImpl "authBackend/internal/app/ports/out/dao"
 	"authBackend/internal/app/ports/out/keycloak"
-	"github.com/gin-gonic/gin"
+	"authBackend/internal/infrastructure/transport/http/presenter"
 )
 
 type AccountController struct {
-	delegate *delegate.AccountDelegate
-}
-
-func (cntr *AccountController) RegisterAccount(c *gin.Context) {
-	//var req openapi.CreateAccountRequest
-	//if err := marshalling.HandleMarshalling(c, &req); err != nil {
-	//	return
-	//}
-	//marshalling.HandleResponse(c, func() (*domain.Account, error) {
-	//	return cntr.delegate.Save(cntr.presenter.PresentToReq(req))
-	//}, cntr.presenter.PresentToResp)
-
-	//var requestEntity generatedApi.CreateAccountRequest
-	//utilities.CatchMarshallErr(context.BindJSON(&requestEntity), context)
-	//entity, err := controller.registerAcc.Register(requestEntity)
-	//controller.processAccountResult(context, err, entity)
-}
-
-func (controller *AccountController) GetAccountByTgId(context *gin.Context, telegramId int64) {
-	//entity, err := controller.getAccByTgId.Get(telegramId)
-	//controller.processAccountResult(context, err, entity)
-}
-
-func (controller *AccountController) processAccountResult(context *gin.Context, err error) {
-	//if err != nil {
-	//	utilities.SetResponseError(context, http.StatusInternalServerError)
-	//} else {
-	//	utilities.SetResponseWithStatus(
-	//		controller.presenter.ToAccountResponse(entity),
-	//		context,
-	//		http.StatusOK,
-	//	)
-	//}
+	delegate  *delegate.AccountDelegate
+	presenter presenter.AccountPresenter
 }
 
 func CreateAccountController(keycloakClient *keycloak.KeycloakClient, dao *daoImpl.AuthDao) *AccountController {
 	return &AccountController{
-		delegate: delegate.CreateAccountDelegate(dao, *keycloakClient),
+		delegate:  delegate.CreateAccountDelegate(dao, *keycloakClient),
+		presenter: presenter.AccountPresenter{},
 	}
 }
